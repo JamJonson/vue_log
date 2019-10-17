2ef48ec09a8f0ce4d7d7e61fcad3cc4e4d24b494
unknown <JamJonson@163.com>
2019-10-17 23:25:59 +0800

    增加发布订阅解释

---
+++ vue2.0/vue_原理解析/发布订阅/index.js
@@ -0,0 +1,31 @@
+// 发布订阅模式 先有订阅再有发布
+
+// 绑定的方法都有update属性
+function Dep () {
+    this.subs = []
+}
+Dep.prototype.addSub = function (sub) { // 订阅
+    this.subs.push(sub)
+}
+
+Dep.prototype.notify = function () { // 通知
+    this.subs.forEach(sub => sub.update()); // 给每个方法加上update属性
+}
+
+// Watcher是一个类 通过这个类创建的实例都拥有update方法
+function Watcher (fn) {
+    this.fn = fn
+}
+
+Watcher.prototype.update =  function () {
+    this.fn()
+}
+
+let watcher = new Watcher(function(){
+    console.log('JamJonson')
+})
+
+
+let dep = new Dep();
+dep.addSub(watcher);
+dep.notify()
